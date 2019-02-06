e688a0d5366a910bea6275c26e0934c22037f40d
Malah <malah@neuf.fr>
2019-02-06 16:02:09 +0100

    corrected key

--- app/src/main/java/fr/istic/hbmlh/photoloc/manager/HttpRequest.java
+++ app/src/main/java/fr/istic/hbmlh/photoloc/manager/HttpRequest.java
@@ -1,17 +1,20 @@
 package fr.istic.hbmlh.photoloc.manager;
 
+import android.content.Context;
 import android.location.Location;
 
+import fr.istic.hbmlh.photoloc.R;
 import fr.istic.hbmlh.photoloc.manager.base.ClientManager;
 import fr.istic.hbmlh.photoloc.manager.base.EntityListener;
 import fr.istic.hbmlh.photoloc.manager.model.Result;
 
 public class HttpRequest {
 
-    private static final HttpRequest instance = new HttpRequest();
 
-    public static HttpRequest getInstance() {
-        return instance;
+    private Context context;
+
+    public HttpRequest(Context context) {
+        this.context = context;
     }
 
     private ClientManager<Result> AdressComponents;
@@ -19,7 +22,7 @@ public class HttpRequest {
 
     public void getAdressFromLocation(Location location, EntityListener<Result> listener) {
         String url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +location.getLatitude() +
-                ","+location.getLongitude()+"&key=AIzaSyCVazRV2NVNLkfKueg3clIrSQiUJ1x2um4";
+                ","+location.getLongitude()+"&key=" + context.getResources().getString(R.string.google_maps_key);
         this.AdressComponents.readOne(listener, url );
     }
 
